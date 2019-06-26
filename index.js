ab60240497ff5cdd01e1e15d4c3e6eb798a378e1
Brandon Fries <brandonfries1993@gmail.com>
2019-06-26 18:06:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('div.target')
 }
 function increaseRankBy(n) {
-  var increaseRank = document.querySelectorAll('increaseRank.ranked-list')
-  for (let i = 0 ; i < increaseRank.length; i ++) {
-    increaseRank[i].textContent = parseInt(increaseRank[i].textContent) + n
-  }
-return increaseRank
+  var increaseRank = parseInt(ranked-list)
+
 }
