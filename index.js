15addaaf99491c43bfb2254672e237198963d298
Brandon Fries <brandonfries1993@gmail.com>
2019-06-26 18:42:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+function increaseRankBy(n) {
+  var increaseRank = document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0 ; i < increaseRank.length; i ++) {
+    increaseRank[i].innerHTML = parseInt(increaseRank[i].innerHTML) + n
+  }
+}
+function deepestChild() {
+  var deep = document.querySelector('div#grand-node').querySelectorAll('div')
+  return deep[deep.length - 1]
+}
